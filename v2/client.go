4cf6e9197d43cef34513e187b4cce78373839a98
tok-kkk <yunshi@catalog.fi>
2025-05-23 12:20:42 +1000

    enable the withdraw quota service

--- v2/client.go
+++ v2/client.go
@@ -787,6 +787,11 @@ func (c *Client) NewWithdrawAddressService() *WithdrawAddressService {
 	return &WithdrawAddressService{c: c}
 }
 
+// NewWithdrawQuotaService init listing withdraw quota service
+func (c *Client) NewWithdrawQuotaService() *WithdrawQuotaService {
+	return &WithdrawQuotaService{c: c}
+}
+
 // NewStartUserStreamService init starting user stream service
 func (c *Client) NewStartUserStreamService() *StartUserStreamService {
 	return &StartUserStreamService{c: c}
